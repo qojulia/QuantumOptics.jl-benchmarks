bceae029c744b93e743b700d45dcb8e57e524786
Sebastian Kraemer <basti.kr@gmail.com>
2017-06-01 12:51:33 +0200

    Restructure matlab multiplication benchmarks.

---
+++ benchmarks-QuantumOpticsToolbox/bench_multiplication_dense_sparse.m
@@ -0,0 +1,30 @@
+
+function results = bench_multiplication_dense_sparse()
+    basename = 'multiplication_dense_sparse';
+    cutoffs = 50:50:1000;
+    S = [0.1, 0.01, 0.001];
+    Nrand = 5;
+    for s = S
+        name = [basename, '_', strrep(mat2str(s), '.', '')];
+        results = [];
+        for N = cutoffs
+            T = 0.;
+            for i=1:Nrand
+                [op1, op2] = setup(N, s);
+                f_ = @() f(op1, op2);
+                T = T + timeit(f_);
+            end
+            results = [results, T/Nrand];
+        end
+        savebenchmark(name, cutoffs, results)
+    end
+end
+
+function [op1, op2] = setup(N, s)
+    op1 = (1.+0.3j)*rand(N, N);
+    op2 = (1.+0.3j)*sprand(N, N, s);
+end
+
+function result = f(op1, op2)
+    result = op1*op2;
+end
