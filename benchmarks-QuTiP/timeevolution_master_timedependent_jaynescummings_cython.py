111293df42601179b9d648dd36c504dacb7771ce
Sebastian Kraemer <basti.kr@gmail.com>
2017-06-04 18:30:45 +0200

    Add timedependent jaynes cummings benchmark.

---
+++ benchmarks-QuTiP/timeevolution_master_timedependent_jaynescummings_cython.py
@@ -0,0 +1,67 @@
+import qutip as qt
+import numpy as np
+import benchmarkutils
+
+name = "timeevolution_master_timedependent_jaynescummings[cython]"
+
+samples = 3
+evals = 3
+cutoffs = range(10, 31, 10)
+
+
+def setup(N):
+    options = qt.Options()
+    options.atol = 1e-8
+    options.rtol = 1e-6
+    return options
+
+
+def f(N, options):
+    wa = 1
+    wc = 0.9
+    delta = wa - wc
+    g = 2
+    kappa = 0.5
+    gamma = 0.1
+    n_th = 0.75
+    tspan = np.linspace(0, 10, 11)
+
+    Ia = qt.qeye(2)
+    Ic = qt.qeye(N)
+
+    a = qt.destroy(N)
+    at = qt.create(N)
+
+    sm = qt.sigmam()
+    sp = qt.sigmap()
+
+    # H = wc*qt.tensor(n, Ia) + qt.tensor(Ic, wa/2.*sz) + g*(qt.tensor(at, sm) + qt.tensor(a, sp))
+    Ha = g * qt.tensor(at, sm)
+    Hb = g * qt.tensor(a, sp)
+    H = [[Ha, 'exp(-1j* {delta}*t)'.format(delta=delta)],
+         [Hb, 'exp(1j*{delta}*t)'.format(delta=delta)]]
+    c_ops = [
+        qt.tensor(np.sqrt(kappa*(1+n_th)) * a, Ia),
+        qt.tensor(np.sqrt(kappa*n_th) * at, Ia),
+        qt.tensor(Ic, np.sqrt(gamma) * sm),
+    ]
+
+    psi0 = qt.tensor(qt.fock(N, 0), (qt.basis(2, 0) + qt.basis(2, 1)).unit())
+    exp_asp = qt.mesolve(H, psi0, tspan, c_ops, [qt.tensor(a, sp)], options=options).expect[0]
+    return np.real(exp_asp)
+
+
+print("Benchmarking:", name)
+print("Cutoff: ", end="", flush=True)
+checks = {}
+results = []
+for N in cutoffs:
+    print(N, "", end="", flush=True)
+    options = setup(N)
+    checks[N] = sum(f(N, options))
+    t = benchmarkutils.run_benchmark(f, N, options, samples=samples, evals=evals)
+    results.append({"N": N, "t": t})
+print()
+
+benchmarkutils.check(name, checks)
+benchmarkutils.save(name, results)
---
+++ benchmarks-QuantumOptics.jl/timeevolution_master_timedependent_jaynescummings.jl
@@ -0,0 +1,63 @@
+using QuantumOptics
+using BenchmarkTools
+include("benchmarkutils.jl")
+
+name = "timeevolution_master_timedependent_jaynescummings"
+
+samples = 3
+evals = 3
+cutoffs = [10:10:60;]
+
+function setup(N)
+    nothing
+end
+
+function f(N)
+    ωa = 1
+    ωc = 0.9
+    Δ = ωa - ωc
+    g = 2
+    κ = 0.5
+    γ = 0.1
+    n_th = 0.75
+    tspan = [0:1:10;]
+
+    b_cavity = FockBasis(N-1)
+    b_atom = SpinBasis(1//2)
+    b = b_cavity ⊗ b_atom
+
+    a = destroy(b_cavity)
+    at = create(b_cavity)
+
+    sm = sigmam(b_atom)
+    sp = sigmap(b_atom)
+
+    # H = embed(b, 1, ωc*n) + embed(b, 2, ωa*sz/2) + g*(at⊗sm+a⊗sp)
+    Ha = g*at⊗sm
+    Hb = g*a⊗sp
+    rates = [κ*(1+n_th), κ*n_th, γ]
+    J = [embed(b, 1, a), embed(b, 1, at), embed(b, 2, sm)]
+    Jdagger = dagger.(J)
+    ψ₀ = fockstate(b_cavity, 0) ⊗ normalize(spinup(b_atom) + spindown(b_atom))
+
+    H(t, rho) = (exp(-1im*Δ*t)*Ha + exp(1im*Δ*t)*Hb, J, Jdagger)
+    exp_asp = Float64[]
+    fout(t, ρ) = push!(exp_asp, real(expect(a⊗sp, ρ)))
+    timeevolution.master_dynamic(tspan, ψ₀, H; Gamma=rates, fout=fout, reltol=1e-6, abstol=1e-8)
+    exp_asp
+end
+
+println("Benchmarking: ", name)
+print("Cutoff: ")
+checks = Dict{Int, Float64}()
+results = []
+for N in cutoffs
+    print(N, " ")
+    checks[N] = sum(f(N))
+    t = @belapsed f($N) samples=samples evals=evals
+    push!(results, Dict("N"=>N, "t"=>t))
+end
+println()
+
+benchmarkutils.check(name, checks)
+benchmarkutils.save(name, results)
