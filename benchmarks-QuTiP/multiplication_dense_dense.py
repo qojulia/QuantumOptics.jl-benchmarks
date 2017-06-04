76eca93818feddb895ed30f762a92b642c761193
Sebastian Kraemer <basti.kr@gmail.com>
2017-06-04 19:26:42 +0200

    Clean up a few benchmarks.

--- benchmarks-QuTiP/multiplication_dense_dense.py
+++ benchmarks-QuTiP/multiplication_dense_dense.py
@@ -7,13 +7,16 @@ samples = 2
 evals = 5
 cutoffs = range(50, 601, 50)
 
+
 def setup(N):
-    op1 = sp.rand(N, N, 1., dtype=float)*0.2j
-    op2 = sp.rand(N, N, 1., dtype=float)*0.1j
+    op1 = sp.rand(N, N, 1., dtype=float) * 0.2j
+    op2 = sp.rand(N, N, 1., dtype=float) * 0.1j
     return op1, op2
 
+
 def f(op1, op2):
-    return op1*op2
+    return op1 * op2
+
 
 print("Benchmarking:", name)
 print("Cutoff: ", end="", flush=True)
--- benchmarks-QuTiP/multiplication_dense_sparse.py
+++ benchmarks-QuTiP/multiplication_dense_sparse.py
@@ -9,13 +9,16 @@ cutoffs = range(50, 1001, 50)
 S = [0.1, 0.01, 0.001]
 Nrand = 5
 
+
 def setup(N, s):
-    op1 = sp.rand(N, N, 1., dtype=float)*0.2j
-    op2 = sp.rand(N, N, s, dtype=float)*0.1j
+    op1 = sp.rand(N, N, 1., dtype=float) * 0.2j
+    op2 = sp.rand(N, N, s, dtype=float) * 0.1j
     return op1, op2
 
+
 def f(op1, op2):
-    return op1*op2
+    return op1 * op2
+
 
 for s in S:
     name = basename + "_" + str(s).replace(".", "")
@@ -27,7 +30,8 @@ for s in S:
         T = 0.
         for i in range(Nrand):
             op1, op2 = setup(N, s)
-            T += benchmarkutils.run_benchmark(f, op1, op2, samples=samples, evals=evals)
-        results.append({"N": N, "t": T/Nrand})
+            T += benchmarkutils.run_benchmark(f, op1, op2,
+                                              samples=samples, evals=evals)
+        results.append({"N": N, "t": T / Nrand})
     print()
     benchmarkutils.save(name, results)
