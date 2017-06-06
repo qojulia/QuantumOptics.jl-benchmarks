94ad8f5c523b5fca12bcb4d4d1c6b613604e49f2
Sebastian Kraemer <basti.kr@gmail.com>
2017-06-06 13:37:56 +0200

    Change order in runall script.

--- runall.py
+++ runall.py
@@ -7,10 +7,6 @@ pythonbenchmarks = "benchmarks-QuTiP"
 
 subprocess.run(["python", "hardware_specs.py"], check=True)
 
-os.chdir(matlabbenchmarks)
-subprocess.run(["matlab", "-nodisplay", "-nosplash", "-nodesktop", "-r", 'run runall.m; quit;'], check=True)
-os.chdir("..")
-
 os.chdir(juliabenchmarks)
 filenames = os.listdir(".")
 for name in filenames:
@@ -25,3 +21,8 @@ for name in filenames:
     if "benchmarkutils" in name or not name.endswith(".py"):
         continue
     subprocess.run(["python", name], check=True)
+os.chdir("..")
+
+os.chdir(matlabbenchmarks)
+subprocess.run(["matlab", "-nodisplay", "-nosplash", "-nodesktop", "-r", 'run runall.m; quit;'], check=True)
+os.chdir("..")
